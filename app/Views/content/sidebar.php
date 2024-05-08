516f9bdb8fdb90c347dd47e8f56b0165340525c7
Valen Rionald <vrionald@gmail.com>
2024-05-08 08:22:52 +0700

    Fix(Sidebar): Menghilangkan template yang tidak diperlukan

--- app/Views/content/sidebar.php
+++ app/Views/content/sidebar.php
@@ -173,18 +173,6 @@
 						</a>
 					</li> -->
 				</ul>
-
-				<div class="sidebar-cta">
-					<div class="sidebar-cta-content">
-						<strong class="d-inline-block mb-2">Upgrade to Pro</strong>
-						<div class="mb-3 text-sm">
-							Are you looking for more components? Check out our premium version.
-						</div>
-						<div class="d-grid">
-							<a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
-						</div>
-					</div>
-				</div>
 			</div>
 		</nav>
 		<!-- Sidebar End -->
